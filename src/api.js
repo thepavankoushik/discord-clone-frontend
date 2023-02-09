432d5c96c426e6c38e6e97481b0ae71040e31756
Pavan Koushik <pavankoushik22@gmail.com>
2023-02-09 13:40:42 -0800

    dasfasdf

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://thepavankoushik-discord-back.herokuapp.com/api",
   timeout: 1000,
 });
 
